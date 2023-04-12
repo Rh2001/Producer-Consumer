f0837b1dcd694b19c93f5ad49e8b7f13b8d28ea0
Roham H <100292931+Rh2001@users.noreply.github.com>
2023-04-12 20:47:51 +0330

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -6,6 +6,7 @@
 */
 
 
+
 namespace Producer_Consumer_No_Semaphore
 {
     internal class Program
@@ -16,57 +17,84 @@ namespace Producer_Consumer_No_Semaphore
 
             Thread t1 = new Thread(new ThreadStart(proCun.producer));
             Thread t2 = new Thread(new ThreadStart(proCun.consumer));
+            
 
             t1.Start();
             t2.Start();
 
-            Thread.Sleep(500);   //execute application for 500 seconds before terminating it
+            Thread.Sleep(5000);   //execute application for 500 seconds before terminating it
+
             Environment.Exit(0);
+            
 
         }
     }
 
 
-    public class ProducerConsumer 
+    public class ProducerConsumer
     {
         int[] buffer = new int[7];    //array acting as a buffer with 7 empty spaces
         int i; // item to be produced
-        int insert=0, extract=0; //in and out
+        int insert = 0, extract = 0; //in and out
         Random random = new Random(); // creating a new randomizer
         public ProducerConsumer() { }
 
-        public void producer() 
+        public void producer()
         {
-            while (true) 
+            Console.WriteLine("Starting producer");
+            
+            while (true)
             {
-                i = random.Next();    //produce a new random number
-                while ((insert + 1) % buffer.Length == extract)
+               
+                i = random.Next(100);    //produce a new random number with the maximum value of 100
+
+                while (((insert + 1) % buffer.Length) == extract)
                 {
-                    Console.WriteLine("Buffer is full ... sleeping");
-                    Thread.Sleep(5);
+                    Console.WriteLine("Waiting on consumer ");
+                    Console.WriteLine();
+                    Thread.Sleep(2);
                 }
+
+
+               
                 buffer[insert] = i;         //add new random 
                 Console.WriteLine("Produced " + buffer[insert]);
-                insert = (insert + 1) % buffer.Length;    //move the insert number by one unit
-                Console.WriteLine("Next index to produce is " + insert);
+                insert = (insert + 1) % buffer.Length;            //move the insert number by one unit
+                Console.WriteLine();   //empty line
+
+                Console.WriteLine("Elements in the buffer: " + buffer[0] + " " + buffer[1] + " " + buffer[2] + " " + buffer[3] + " " + buffer[4] 
+                    + " " + buffer[5] + " " + buffer[6]);
+
+                Console.WriteLine();
+                Thread.Sleep(400);
+
+
             }
         }
 
-        public void consumer() 
+        public void consumer()
         {
-          
-            while (true) 
+            Console.WriteLine("Starting Consumer");
+            Thread.Sleep(2);
+            while (true)
             {
-                while (insert == extract)  //buffer is empty
+               
+               while (insert == extract)  //buffer is empty
                 {
-                    Console.WriteLine("Buffer is empty! sleeping");
-                    Thread.Sleep(5);
+                   Console.WriteLine("Waiting on producer... buffer is empty");
+                   Thread.Sleep(1);
                 }
                 Console.WriteLine("Consumed " + buffer[extract]);
+                Console.WriteLine();
+
+            
                 buffer[extract] = 0;
+                Console.WriteLine("Elements in the buffer: " + buffer[0] + " " + buffer[1] + " " + buffer[2] + " " + buffer[3] + " " + buffer[4]
+                   + " " + buffer[5] + " " + buffer[6]);
+
                 extract = (extract + 1) % buffer.Length;   //move the out index by one... the % is because we want to go back to the beginning of the buffer
-               
-             
+
+                Thread.Sleep(500);
             }
         }
     }
